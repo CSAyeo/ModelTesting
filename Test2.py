53f954b5771b0e5bac38b7c141b4c3c0590e9eb7
Chris <17007976@brookes.ac.uk>
2024-01-07 01:19:56 +0000

    doesn't work now lamo

--- Test2.py
+++ Test2.py
@@ -2,6 +2,7 @@ import time
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from keras.src.layers import RepeatVector
 from tensorflow.keras.layers import Input, Dense, LSTM
 from tensorflow.keras.optimizers import Adam
 from sklearn.metrics import mean_squared_error
@@ -61,6 +62,107 @@ class ARIMAModel(BaseModel):
     def predict(self, X_test):
         y_pred = self.model_fit.predict(start=len(X_train), end=len(X_train) + len(X_test) - 1, typ='levels')
         return y_pred
+
+class GANModel():
+    def __init__(self, epochs, batch_size, latent_dim):
+        self.epochs = epochs
+        self.batch_size = batch_size
+        self.latent_dim = latent_dim
+        self.generator = None
+        self.discriminator = None
+        self.model = None
+        self.scaler_X = MinMaxScaler()
+        self.scaler_y = MinMaxScaler()
+
+    def sampling(self, args):
+        z_mean, z_log_var = args
+        batch = K.shape(z_mean)[0]
+        dim = K.int_shape(z_mean)[1]
+        epsilon = K.random_normal(shape=(batch, dim))
+        return z_mean + K.exp(0.5 * z_log_var) * epsilon
+
+    def vae_loss(self, x, x_decoded_mean):
+        x = K.flatten(x)
+        x_decoded_mean = K.flatten(x_decoded_mean)
+        xent_loss = self.input_dim * K.mean(K.square(x - x_decoded_mean))
+        kl_loss = -0.5 * K.sum(1 + self.z_log_var - K.square(self.z_mean) - K.exp(self.z_log_var), axis=-1)
+        return xent_loss + kl_loss
+
+    def build_generator(self):
+        model = Sequential()
+        model.add(LSTM(50, activation='relu', return_sequences=True, input_shape=(1, 1)))
+        model.add(LSTM(50, activation='relu', return_sequences=True))
+        model.add(LSTM(50, activation='relu', return_sequences=True))
+        model.add(LSTM(50, activation='relu', return_sequences=True))
+        model.add(LSTM(50, activation='relu', return_sequences=False))
+        model.add(Dense(1))
+        optimizer = Adam(learning_rate=0.001)
+        model.compile(optimizer=optimizer, loss='mse')
+        self.generator = model
+
+    def build_discriminator(self):
+        model = Sequential()
+        model.add(LSTM(50, activation='relu', return_sequences=True, input_shape=(1, 1)))
+        model.add(LSTM(50, activation='relu', return_sequences=True))
+        model.add(LSTM(50, activation='relu', return_sequences=True))
+        model.add(LSTM(50, activation='relu', return_sequences=True))
+        model.add(LSTM(50, activation='relu', return_sequences=True))  # Adjusted this layer
+        model.add(Dense(1, activation='sigmoid'))
+        optimizer = Adam(learning_rate=0.001)
+        model.compile(optimizer=optimizer, loss='binary_crossentropy')
+        self.discriminator = model
+
+    def build_gan(self):
+        self.discriminator.trainable = False
+        gan_input = self.generator.input
+        generated_sequence = self.generator(gan_input)
+        # Adjust the shape of generated_sequence
+        generated_sequence = RepeatVector(self.input_dim)(generated_sequence)
+        validity = self.discriminator(generated_sequence)
+        self.model = Sequential([self.generator, self.discriminator])
+        optimizer = Adam(learning_rate=0.001)
+        self.model.compile(optimizer=optimizer, loss=['mse', 'binary_crossentropy'])
+
+    def train(self, X_train, y_train):
+        if isinstance(X_train, pd.DatetimeIndex):
+            # If X_train is a DatetimeIndex, use the index as a feature
+            X_train = np.arange(len(X_train)).reshape(-1, 1)
+        self.input_dim = X_train.shape[1]
+
+        X_train_scaled = self.scaler_X.fit_transform(X_train)
+        y_train_scaled = self.scaler_y.fit_transform(y_train.values.reshape(-1, 1))
+
+        X_train_scaled = X_train_scaled.reshape((X_train_scaled.shape[0], 1, X_train_scaled.shape[1]))
+
+        self.build_generator()
+        self.build_discriminator()
+        self.build_gan()
+
+        for epoch in range(self.epochs):
+            # Train discriminator
+            idx = np.random.randint(0, X_train_scaled.shape[0], self.batch_size)
+            real_sequences = X_train_scaled[idx]
+            valid = np.ones((self.batch_size, 1))
+            fake_sequences = self.generator.predict(real_sequences)
+            fake = np.zeros((self.batch_size, 1))
+            print("Real sequences shape:", real_sequences.shape)
+            print("Fake sequences shape:", fake_sequences.shape)
+            d_loss_real = self.discriminator.train_on_batch(real_sequences, valid)
+            fake_sequences_reshaped = fake_sequences.reshape((fake_sequences.shape[0], fake_sequences.shape[1], 1))
+            d_loss_fake = self.discriminator.train_on_batch(fake_sequences_reshaped, fake)
+            d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
+
+            # Train generator
+            g_loss = self.model.train_on_batch(real_sequences, [real_sequences, valid])
+
+    def predict(self, X_test):
+        X_test_scaled = self.scaler_X.transform(X_test.values.reshape(-1, 1))
+        X_test_scaled = X_test_scaled.reshape((X_test_scaled.shape[0], 1, X_test_scaled.shape[1]))
+
+        y_pred_scaled = self.generator.predict(X_test_scaled)
+        y_pred = self.scaler_y.inverse_transform(y_pred_scaled)
+
+        return y_pred.flatten()
 
 class ETSModel(BaseModel):
     def __init__(self, seasonal_periods):
@@ -156,9 +258,11 @@ X_test, y_test = test_data.index, test_data['value']
 lstm_params = {'epochs': [50, 100], 'batch_size': [32, 64]}
 arima_params = {'order': [(5, 1, 2), (3, 1, 1)]}
 ets_params = {'seasonal_periods': [30, 40]}
+gan_params = {'epochs': [50], 'batch_size': [32], 'latent_dim': [100]}  # GAN hyperparameters
 
 # Evaluate models with different parameters
 models_to_test = {
+   # 'GAN': (GANModel, gan_params),  # Include GAN in the models to test
     'LSTM': (LSTMModel, lstm_params),
     'ARIMA': (ARIMAModel, arima_params),
     'ETS': (ETSModel, ets_params),
@@ -172,24 +276,26 @@ for model_name, (model_class, model_params) in models_to_test.items():
 
 # Create a DataFrame from the mse_runtimes list
 df = pd.DataFrame(mse_runtimes, columns=['Model', 'Params', 'MSE', 'Runtime'])
-
 # Flatten the parameters for better display
 df['Params'] = df['Params'].apply(lambda x: ', '.join(f'{key}={value}' for key, value in x.items()))
-
+print(df)
 # Create a scatter plot using Seaborn
 plt.figure(figsize=(12, 8))
 
 # Define custom color palette with varying hues for each model
 custom_palette = sns.color_palette("husl", n_colors=len(df['Model'].unique()))
-
+print(custom_palette)
 # Scatter plot with transparency for individual runs
 scatter = sns.scatterplot(x="Runtime", y="MSE", hue="Model", style="Params", data=df,
-                          palette=custom_palette, s=100, markers=True, alpha=0.3)
+                          palette=custom_palette, s=100, markers=True, alpha=0.5)
 
 # Plot means for each parameter in solid color
 means_df = df.groupby(['Model', 'Params']).mean().reset_index()
+print(means_df)
+custom_palette = sns.color_palette("husl", n_colors=len(means_df['Model'].unique()))
+print(custom_palette)
 sns.scatterplot(x="Runtime", y="MSE", hue="Model", style="Params", data=means_df,
-                palette=custom_palette, s=150, markers=True, legend=False, alpha=1.0)
+                palette=custom_palette, s=150, markers=True, legend=False)
 
 # Set legend outside the plot
 scatter.legend(loc='upper left', bbox_to_anchor=(1, 1))
